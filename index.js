cce2a59b8b3b836a4052554b091ce255b42638c6
zine eddine <iz_zidane@esi.dz>
2021-08-17 20:12:34 +0100

    intial commit | project setup

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+
+dotenv.config({ path: './config/config.env' });
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.json({ message: "test" });
+})
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server is running in ${process.env.NODE_ENV} mode on ${PORT}`));
